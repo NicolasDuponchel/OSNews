aa376a1551b286a9ef28f9461a7357a6e0b46178
NicolasDuponchel <nicolas.duponchel@gmail.com>
2022-08-30 15:45:13 +0200

    AS project creation

---
+++ app/src/main/java/com/ndup_esiee/osnews/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.ndup_esiee.osnews
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.lifecycle.lifecycleScope
+import com.ndup_esiee.osnews.repository.NYTServiceFactory
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            //TODO NDU
+        }
+    }
+
+
+    }
+}
