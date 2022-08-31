340dbfc885d30469000f56d085122e41883f5304
NicolasDuponchel <nicolas.duponchel@gmail.com>
2022-08-31 10:01:45 +0200

    Introduce NYT services

---
+++ app/src/main/java/com/ndup_esiee/osnews/repository/NYTServiceFactory.kt
@@ -0,0 +1,38 @@
+package com.ndup_esiee.osnews.repository
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+@OptIn(ExperimentalSerializationApi::class)
+object NYTServiceFactory {
+
+    private const val TimesNewsWireBaseUrl = "https://api.nytimes.com/svc/news/v3/content/"
+    private const val ApiKey = "ad1ztM4z97DlnqQtloL9nWfHLNhB3Jew"
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(TimesNewsWireBaseUrl)
+        .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+        .client(client)
+        .build()
+
+    private val client get() = OkHttpClient.Builder()
+        .addInterceptor { apiKeyAsQuery(it) }
+        .addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY })
+        .build()
+
+    private fun apiKeyAsQuery(chain: Interceptor.Chain) = chain.proceed(
+        chain.request()
+            .newBuilder()
+            .url(chain.request().url.newBuilder().addQueryParameter("api-key", ApiKey).build())
+            .build()
+    )
+
+    val service: NYTServices = retrofit.create(NYTServices::class.java)
+
+}
