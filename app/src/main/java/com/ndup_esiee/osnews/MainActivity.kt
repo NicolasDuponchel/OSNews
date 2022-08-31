501684e19f64afb8f9b8739e67394f7e38492867
NicolasDuponchel <nicolas.duponchel@gmail.com>
2022-08-31 11:16:05 +0200

    Injection with Koin introduction

--- app/src/main/java/com/ndup_esiee/osnews/MainActivity.kt
+++ app/src/main/java/com/ndup_esiee/osnews/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.lifecycle.lifecycleScope
-import com.ndup_esiee.osnews.repository.NYTServiceFactory
+import com.ndup_esiee.osnews.repository.INYTRepository
 import kotlinx.coroutines.launch
+import org.koin.android.ext.android.inject
 
 class MainActivity : ComponentActivity() {
+
+    private val nytRepository: INYTRepository by inject()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
