05cf7047bfb560e9472c494e40aff889702d3f06
NicolasDuponchel <nicolas.duponchel@gmail.com>
2022-08-31 18:59:11 +0200

    WIP on UI

--- app/src/main/java/com/ndup_esiee/osnews/MainActivity.kt
+++ app/src/main/java/com/ndup_esiee/osnews/MainActivity.kt
@@ -3,8 +3,23 @@ package com.ndup_esiee.osnews
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.lifecycleScope
 import com.ndup_esiee.osnews.repository.INYTRepository
+import com.ndup_esiee.osnews.repository.model.NewsWires
+import com.ndup_esiee.osnews.repository.model.Section
+import com.ndup_esiee.osnews.repository.model.Sections
+import com.ndup_esiee.osnews.repository.ui.NewsWireGrid
+import com.ndup_esiee.osnews.repository.ui.SectionCells
+import com.ndup_esiee.osnews.repository.ui.newsWiresTestList
+import com.ndup_esiee.osnews.repository.ui.sectionsTestList
 import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 
@@ -15,10 +30,49 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            //TODO NDU
+            var sections: Sections by remember { mutableStateOf(emptyList()) }
+            LaunchedEffect(sections) { sections = nytRepository.getSections() }
+
+            var newsWires: NewsWires by remember { mutableStateOf(emptyList()) }
+            LaunchedEffect(newsWires) {
+                newsWires = nytRepository.getNews(DEFAULT_SECTION)
+                println(newsWires)
+            }
+
+            mainContent(sections, newsWires)
         }
     }
 
-
+    @Composable
+    private fun mainContent(
+        sections: Sections,
+        newsWires: NewsWires
+    ) {
+//        Column(
+//                            modifier = Modifier.fillMaxHeight(),
+//            horizontalAlignment = Alignment.CenterHorizontally,
+//            verticalArrangement = Arrangement.Center,
+//        ) {
+            NewsWireGrid(newsWires, Modifier.fillMaxHeight())
+            SectionCells(sections)
+//        }
     }
+
+    @Composable
+    @Preview
+    private fun mainPreview() {
+        mainContent(sections = sectionsTestList, newsWires = newsWiresTestList)
+    }
+
+    private fun testWebCall() {
+        lifecycleScope.launch {
+            println(nytRepository.getNews(DEFAULT_SECTION))
+        }
+    }
+
+    companion object {
+        private val DEFAULT_SECTION = Section("home page", "Home Page")
+    }
+
+
 }
---
+++ app/src/main/java/com/ndup_esiee/osnews/repository/ui/SectionView.kt
@@ -0,0 +1,117 @@
+package com.ndup_esiee.osnews.repository.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ndup_esiee.osnews.repository.model.Section
+import com.ndup_esiee.osnews.repository.model.Sections
+
+@Composable
+fun SectionCell(section: Section) {
+    val roundedShape = RoundedCornerShape(8.dp)
+    Text(
+        modifier = Modifier
+            .background(
+                color = Color.White,
+                shape = roundedShape,
+            )
+            .border(
+                width = 1.dp,
+                color = Color.Black,
+                shape = roundedShape
+            )
+            .padding(horizontal = 8.dp, vertical = 6.dp)
+            .wrapContentSize(),
+        text = section.displayName,
+        textAlign = TextAlign.Center,
+    )
+}
+
+@Composable
+fun SectionCells(
+    list: Sections,
+    modifier: Modifier = Modifier,
+) {
+    LazyRow(
+        modifier = modifier.padding(vertical = 12.dp),
+        contentPadding = PaddingValues(start = 12.dp, end = 12.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+    ) {
+        items(items = list) { SectionCell(section = it) }
+    }
+}
+
+
+@Composable
+@Preview
+private fun preview() {
+    Column {
+        SectionCells(
+            sectionsTestList,
+//            Modifier.background(Color.Blue)
+        )
+    }
+}
+
+val sectionsTestList = listOf(
+    "Admin",
+    "Arts",
+    "Automobiles",
+    "Books",
+    "Briefing",
+    "Business",
+    "Climate",
+    "Corrections",
+    "Crosswords & Games",
+    "Education",
+    "En Español",
+    "Fashion",
+    "Food",
+    "Guides",
+    "Health",
+    "Home & Garden",
+    "Home Page",
+    "Job Market",
+    "Lens",
+    "Magazine",
+    "Movies",
+    "Multimedia/Photos",
+    "New York",
+    "Obituaries",
+    "Opinion",
+    "Parenting",
+    "Podcasts",
+    "Reader Center",
+    "Real Estate",
+    "Science",
+    "Smarter Living",
+    "Sports",
+    "Style",
+    "Sunday Review",
+    "T Brand",
+    "T Magazine",
+    "Technology",
+    "The Learning Network",
+    "The Upshot",
+    "The Weekly",
+    "Theater",
+    "Times Insider",
+    "Today’s Paper",
+    "Travel",
+    "U.S.",
+    "Universal",
+    "Video",
+    "Well",
+    "World",
+    "Your Money",
+).map { Section(it.lowercase(), it) }
