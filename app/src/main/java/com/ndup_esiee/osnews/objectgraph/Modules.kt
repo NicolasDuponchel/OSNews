73aa21376a039627c52999b76706ea3c6343c1fd
NicolasDuponchel <nicolas.duponchel@gmail.com>
2022-09-01 18:11:18 +0200

    ViewModel: Migrate presentation logic in android ViewModel

--- app/src/main/java/com/ndup_esiee/osnews/objectgraph/Modules.kt
+++ app/src/main/java/com/ndup_esiee/osnews/objectgraph/Modules.kt
@@ -1,12 +1,22 @@
 package com.ndup_esiee.osnews.objectgraph
 
+import com.ndup_esiee.osnews.presentation.IMainListener
+import com.ndup_esiee.osnews.presentation.MainModel
+import com.ndup_esiee.osnews.presentation.MainViewModel
 import com.ndup_esiee.osnews.repository.INYTRepository
 import com.ndup_esiee.osnews.repository.NYTApiServices
 import com.ndup_esiee.osnews.repository.NYTRepository
 import com.ndup_esiee.osnews.repository.NYTServiceFactory
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val repositoryModule = module {
     single<INYTRepository> { NYTRepository() }
     single<NYTApiServices> { NYTServiceFactory.service }
+}
+
+val presentationModule = module {
+    single<MainModel> { MainModel(sections = emptyList(), newsWires = emptyList()) }
+    viewModel { MainViewModel(get(), get()) } bind IMainListener::class
 }
